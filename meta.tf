a385d5e2dfe1ad128f20f05db2442da12f3653c8
Jaysues <jaysues@gmail.com>
2022-03-01 11:27:38 +1100

    Added changes as required

--- meta.tf
+++ meta.tf
@@ -1,8 +1,8 @@
 terraform {
   required_version = ">= 0.13.0"
   backend "s3" {
-    bucket = "pathways-dojo"
-    key    = "<username>-tfstate-main"
+    bucket = "jaysues-pathways-dojo"
+    key    = "jaysues-tfstate-main"
     region = "us-east-1"
   }
 }
