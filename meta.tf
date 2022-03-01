7a92f7933194f4bcbe6a3f40debbfca0f5f80e79
Justin <jaysues@gmail.com>
2022-03-01 11:20:39 +1100

    Update meta.tf

--- meta.tf
+++ meta.tf
@@ -1,7 +1,7 @@
 terraform {
   required_version = ">= 0.13.0"
   backend "s3" {
-    bucket = "pathways-dojo"
+    bucket = "jaysues-pathways-dojo"
     key    = "jaysues-tfstate-main"
     region = "us-east-1"
   }
